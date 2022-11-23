fe4a5a972050b7d7d4a23e4e57589e6ed8d5933e
Drew Stinnett <drew@drewlink.com>
2022-11-23 10:31:06 -0500

    feature(diary): Adding new filters

--- diary.go
+++ diary.go
@@ -10,3 +10,73 @@ type DiaryEntry struct {
 	Film          *Film
 	Slug          *string
 }
+
+type DiaryEntries []DiaryEntry
+
+type DiaryFilterOpts struct {
+	Earliest      *time.Time
+	Latest        *time.Time
+	Rewatch       *bool
+	SpecifiedDate *bool
+}
+
+type (
+	DiaryFilter     func(DiaryEntry, DiaryFilterOpts) bool
+	DiaryFilterBulk func(DiaryEntries, DiaryFilterOpts) DiaryEntries
+)
+
+func DiaryFilterEarliest(e DiaryEntry, f DiaryFilterOpts) bool {
+	if f.Earliest == nil {
+		return true
+	}
+	return e.Watched.After(*f.Earliest)
+}
+
+func DiaryFilterLatest(e DiaryEntry, f DiaryFilterOpts) bool {
+	if f.Latest == nil {
+		return true
+	}
+	return e.Watched.Before(*f.Latest)
+}
+
+func DiaryFilterRewatch(e DiaryEntry, f DiaryFilterOpts) bool {
+	if f.Rewatch == nil {
+		return true
+	}
+	return *f.Rewatch == e.Rewatch
+}
+
+func DiaryFilterDateSpecified(e DiaryEntry, f DiaryFilterOpts) bool {
+	if f.SpecifiedDate == nil {
+		return true
+	}
+	return *f.SpecifiedDate == e.SpecifiedDate
+}
+
+func ApplyDiaryFilters(records DiaryEntries, opts DiaryFilterOpts, filters ...DiaryFilter) DiaryEntries {
+	// Make sure there are actually filters to be applied.
+	if len(filters) == 0 {
+		return records
+	}
+
+	filteredRecords := make(DiaryEntries, 0, len(records))
+
+	// Range over the records and apply all the filters to each record.
+	// If the record passes all the filters, add it to the final slice.
+	for _, r := range records {
+		keep := true
+
+		for _, f := range filters {
+			if !f(r, opts) {
+				keep = false
+				break
+			}
+		}
+
+		if keep {
+			filteredRecords = append(filteredRecords, r)
+		}
+	}
+
+	return filteredRecords
+}
--- diary_test.go
+++ diary_test.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"os"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/require"
 )
@@ -23,4 +24,74 @@ func TestExtractUserDiary(t *testing.T) {
 
 	require.NotNil(t, items[0].Film)
 	require.Equal(t, "Sweet Sweetback's Baadasssss Song", items[0].Film.Title)
+}
+
+func TestFilterEarliest(t *testing.T) {
+	require.Equal(t, true, DiaryFilterEarliest(DiaryEntry{}, DiaryFilterOpts{}))
+
+	et, _ := time.Parse("2006-01-02", "2020-01-29")
+	e := DiaryEntry{
+		Watched: &et,
+	}
+	ft, _ := time.Parse("2006-01-02", "2021-01-29")
+	f := DiaryFilterOpts{
+		Earliest: &ft,
+	}
+	require.Equal(t, false, DiaryFilterEarliest(e, f))
+}
+
+func TestFilterLatest(t *testing.T) {
+	require.Equal(t, true, DiaryFilterLatest(DiaryEntry{}, DiaryFilterOpts{}))
+
+	et, _ := time.Parse("2006-01-02", "2020-01-29")
+	e := DiaryEntry{
+		Watched: &et,
+	}
+	ft, _ := time.Parse("2006-01-02", "2019-01-29")
+	f := DiaryFilterOpts{
+		Latest: &ft,
+	}
+	require.Equal(t, false, DiaryFilterLatest(e, f))
+}
+
+func TestFilterRewatch(t *testing.T) {
+	truthy := true
+	require.Equal(t, true, DiaryFilterRewatch(DiaryEntry{}, DiaryFilterOpts{}))
+	require.Equal(t, true, DiaryFilterRewatch(
+		DiaryEntry{
+			Rewatch: true,
+		},
+		DiaryFilterOpts{
+			Rewatch: &truthy,
+		},
+	))
+}
+
+func TestFilterSpecifiedDate(t *testing.T) {
+	truthy := true
+	require.Equal(t, true, DiaryFilterDateSpecified(DiaryEntry{}, DiaryFilterOpts{}))
+	require.Equal(t, true, DiaryFilterDateSpecified(
+		DiaryEntry{
+			SpecifiedDate: true,
+		},
+		DiaryFilterOpts{
+			SpecifiedDate: &truthy,
+		},
+	))
+}
+
+func TestApplyDiaryFilters(t *testing.T) {
+	t1, _ := time.Parse("2006-01-02", "2019-01-29")
+	t2, _ := time.Parse("2006-01-02", "2021-01-29")
+	ft, _ := time.Parse("2006-01-02", "2020-01-29")
+	got := ApplyDiaryFilters(
+		DiaryEntries{
+			{Watched: &t1},
+			{Watched: &t2},
+		},
+		DiaryFilterOpts{
+			Earliest: &ft,
+		},
+		DiaryFilterEarliest)
+	require.Equal(t, 1, len(got))
 }
